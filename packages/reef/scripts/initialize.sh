137a31113fa65986382d2409c31ad471b67062d8
draco <136370233+0xDraco@users.noreply.github.com>
2025-11-28 23:17:43 +0100

    remove topic setting in initialize script

--- packages/reef/scripts/initialize.sh
+++ packages/reef/scripts/initialize.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <reef_package_id> <publisher_address> <initial_supported_coin_type>" >&2
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <reef_package_id> <publisher_address>" >&2
     exit 1
 fi
 
 REEF_PKG="$1"
 PUBLISHER="$2"
-COIN_TYPE="$3"
 
 sui client ptb \
     --move-call "$REEF_PKG::protocol::initialize" \
@@ -16,15 +15,6 @@ sui client ptb \
         --assign protocol \
     --move-call 0x2::tx_context::sender \
         --assign sender \
-    --move-call "$REEF_PKG::protocol::add_supported_coin_type" \
-        "<$COIN_TYPE>" \
-        protocol.0 \
-        protocol.1 \
-    --move-call "$REEF_PKG::protocol::set_resolver_fee" \
-        "<$COIN_TYPE>" \
-        protocol.0 \
-        protocol.1 \
-        "0" \
     --move-call "$REEF_PKG::protocol::transfer_cap" \
         protocol.1 \
         sender \
